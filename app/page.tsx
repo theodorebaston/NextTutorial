f832c8596c3c0981c332c173c8dd6e20c0310cc8
theodorebaston <theodorebaston@gmail.com>
2025-01-28 10:21:38 -0500

    Chapter 4: updated file structure for page routing

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Hero Images */}
           <Image
             src="/hero-desktop.png"
             width={1000}
